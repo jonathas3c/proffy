8b1d7d5f3ebc442aac7bffd7a84c734d05112996
Jonathas Germano <jonathas.germano@hotmail.com>
2020-08-11 14:59:19 +0530

    Created TeacherList and TeachForm pages, added react-router-dom for providing Router support and also started to build PageHeader, first of the components to be shared through more than one page

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+import PageHeader from '../../components/PageHeader'
+
+
+function TeacherList() {
+    return(
+        <div id="page-teacher-list" className="container">
+           <PageHeader />
+        </div>
+    )
+}
+
+export default TeacherList;
